7f9d5e12ac8b3bab1836fd32dc2567c2bd5aa1d7
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-04-24 02:20:39 +0300

    Add post and hashtag model

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils.text import slugify
+
+
+class Hashtag(models.Model):
+    tag = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.tag
+
+
+def content_picture_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    return os.path.join(
+        "content_pictures/",
+        f"{slugify(instance.title)}-{uuid.uuid4()}{extension}",
+    )
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()
+    photos = models.ImageField(blank=True, null=True, upload_to=content_picture_file_path)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    hashtag = models.ManyToManyField(Hashtag, related_name="posts")
+
+    def __str__(self):
+        return self.title
