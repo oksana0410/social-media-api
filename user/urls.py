7a700f8aa95552b61a451419b6316a5a3fbe5b34
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-04-24 01:05:24 +0300

    Add logout endpoint

--- user/urls.py
+++ user/urls.py
@@ -5,14 +5,15 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from user.views import CreateUserView, ManageUserView
+from user.views import CreateUserView, ManageUserView, LogoutView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManageUserView.as_view(), name="manage"),
+    path("logout/", LogoutView.as_view(), name="auth_logout"),
 ]
 
 app_name = "user"
