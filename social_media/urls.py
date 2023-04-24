72c94268de7bf92d201e1c14d54ed17feec83974
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-04-24 09:29:19 +0300

    Modify projects and app urls

--- social_media/urls.py
+++ social_media/urls.py
@@ -1,1 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import PostViewSet, CommentViewSet
+
+router = routers.DefaultRouter()
+router.register("posts", PostViewSet)
+router.register("posts/?<post_id>/comments", CommentViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
 app_name = "social-media"
--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -23,6 +23,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
-    #path("api/social-media/", include("social_media.urls", namespace="social-media")),
+    path("api/social-media/", include("social_media.urls", namespace="social-media")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
