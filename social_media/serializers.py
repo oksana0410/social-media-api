4b1747d049c9badb3b3b38c1b5a44c77231af836
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-04-24 09:08:52 +0300

    Add serializers for comment, like, hashtag and post

--- social_media/serializers.py
+++ social_media/serializers.py
@@ -1,9 +1,73 @@
 from rest_framework import serializers
 
-from social_media.models import Hashtag
+from social_media.models import Hashtag, Post, Comment, Like
 
 
 class HashtagSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Hashtag
-        fields = ("id", "name")
+        fields = ("id", "tag")
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ("id", "user", "comment", "created_at")
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        many=True,
+        slug_field="email",
+        read_only=True
+    )
+
+    class Meta:
+        model = Like
+        fields = ("id", "user")
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ("id", "title", "content", "photos", "user", "hashtag")
+        read_only_fields = ("id", "user")
+
+
+class PostListSerializer(serializers.ModelSerializer):
+    hashtags = serializers.SlugRelatedField(
+        slug_field="tag", read_only=True, many=True
+    )
+    likes = serializers.IntegerField(source="likes.count")
+
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "title",
+            "content",
+            "photos",
+            "user",
+            "hashtags",
+            "likes"
+        )
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    likes = LikeSerializer(many=True)
+    comments = CommentSerializer(many=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "title",
+            "content",
+            "photos",
+            "user",
+            "comments",
+            "likes"
+        )
